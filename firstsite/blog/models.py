bcacd67483bffb06dc0943de4c435888a3809fbe
Roman_Tretiakov <romantre@wizardsdev.com>
2024-12-11 23:02:35 +0200

    updated

--- firstsite/blog/models.py
+++ firstsite/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -21,14 +23,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)  # Дата создания статьи
     updated = models.DateTimeField(auto_now_add=True)  # дата и период, когда статья была отредактирована
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')  # статус статьи
-    objects = models.Manager() # Менеджер по умолчанию
-    published = PublishedManager() # Наш новый менеджер
+    objects = models.Manager()  # Менеджер по умолчанию
+    published = PublishedManager()  # Наш новый менеджер
+
+    def get_absolute_url(self):
+        # noinspection PyUnresolvedReferences
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day,
+                                                 self.slug])
 
     class Meta:
         ordering = ('-publish',)  # метаданные в порядке убывания (префикс -)
 
     def __str__(self):
         return self.title  # Возвращает отображение, понятное для человека
-
-
 
