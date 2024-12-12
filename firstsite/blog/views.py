cf74c305b88d024c005076dfcddfd4e3efa79fa7
Roman_Tretiakov <romantre@wizardsdev.com>
2024-12-12 17:38:51 +0200

    updated with modelForm

--- firstsite/blog/views.py
+++ firstsite/blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 
 # Create your views here.
@@ -29,7 +29,27 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # список активных комментариев для этой статьи:
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # пользователь отправил комментарий:
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # создаем комментарий, но пока не сохраняем в базе данных:
+            new_comment = comment_form.save(commit=False)
+            # привязываем комментарий к текущей статье
+            new_comment.post = post
+            # сохраняем комментарий к текущей статье
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 def post_share(request, post_id):
