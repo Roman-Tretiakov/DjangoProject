13a071655bef7b5cf82ae11633e90f1cb610dbf2
Roman_Tretiakov <romantre@wizardsdev.com>
2024-12-13 15:48:33 +0200

    updated code

--- firstsite/blog/models.py
+++ firstsite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -25,17 +26,17 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')  # статус статьи
     objects = models.Manager()  # Менеджер по умолчанию
     published = PublishedManager()  # Наш новый менеджер
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         # noinspection PyUnresolvedReferences
-        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day,
-                                                 self.slug])
-
-    class Meta:
-        ordering = ('-publish',)  # метаданные в порядке убывания (префикс -)
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def __str__(self):
         return self.title  # Возвращает отображение, понятное для человека
+
+    class Meta:
+        ordering = ('-publish',)  # метаданные в порядке убывания (префикс -)
 
 
 class Comment(models.Model):
